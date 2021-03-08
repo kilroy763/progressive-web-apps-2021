8821b6ac515d782afca97424ac554ebf2960b98d
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2021-03-08 15:56:07 +0100

    x

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,8 +3,10 @@ import {fetchData} from './modules/fetcher.js'
 import {render} from './modules/render.js'
 import {router} from './modules/router.js'
 
+const fetch = require("node-fetch");
 
-// Alles laten runnen
+
+// Alles laten runnenbrowserify 
 (async function Init() {
     const dataBrBa = await fetchData(false)
     const dataSaul = await fetchData(true)
