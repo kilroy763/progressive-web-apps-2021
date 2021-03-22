edf87f9d84386439dc37faf7d7a28e9261db2093
kilroy763 <itzmaxkiljan@hotmail.com>
2021-03-22 14:09:37 +0100

    service worker werkt

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ app.get('/breakingbad/season/:season_number', render.seasonBrBa)
 app.get('/bettercallsaul', render.Saul);
 app.get('/bettercallsaul/season/:season_number', render.seasonSaul)
 
+app.get('/offline', function(req, res) {
+	// Send a plain string using res.send();
+	res.render('offline', {
+	})
+});
+
 app.get('*', function(req, res) {
 	// Send a plain string using res.send();
 	res.render('notfound', {
@@ -43,11 +49,7 @@ app.get('*', function(req, res) {
 });
 
 
-app.get('/offline', function(req, res) {
-	// Send a plain string using res.send();
-	res.render('offline', {
-	})
-});
+
 
 
 // Actually set up the server
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const CORE_CACHE = 1
 const CORE_CACHE_NAME = `core-v${CORE_CACHE}`
-const CORE_ASSETS = ["manifest.json", "/offline", "css/style.css"] 
+const CORE_ASSETS = ["manifest.json", "/offline", "css/style.css", "img/background.jpg", "img/brbalogo.png"] 
+
 
 self.addEventListener('install', (event) => {
     console.log("Service worker is geinstalleerd")
